8afaaa0ab20d28c326df51d050585f4fc8c94aa6
Kazuyoshi Akaishi <kazuyoshi_akaishi@mail.toyota.co.jp>
2023-04-19 18:51:13 +0900

    pagesにデプロイした先で正しく画面遷移できるようにBrowserRouterにbasenameを渡した
    Signed-off-by: Shion Koga <shion_koga@mail.toyota.co.jp>
    Co-authored-by: Shion Koga <shion_koga@mail.toyota.co.jp>

--- src/AppNavigation.tsx
+++ src/AppNavigation.tsx
@@ -1,10 +1,11 @@
 import {BrowserRouter} from 'react-router-dom'
 import App from './App'
+import {PUBLIC_URL} from './constants'
 
 
 export default function AppNavigation() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={PUBLIC_URL}>
             <App/>
         </BrowserRouter>
     )

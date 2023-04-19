03e1013dc8a5e05d4c7de3e573dfd922eb32dae1
Kazuyoshi Akaishi <kazuyoshi_akaishi@mail.toyota.co.jp>
2023-04-19 19:35:53 +0900

    pagesにデプロイした先で正しく画面遷移できるようにした
    Signed-off-by: Shion Koga <shion_koga@mail.toyota.co.jp>
    Co-authored-by: Shion Koga <shion_koga@mail.toyota.co.jp>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import styles from './App.module.scss'
 import pokemonLogo from './images/pokemon-logo.png'
-import {Route, Routes} from 'react-router-dom'
+import {Link, Route, Routes} from 'react-router-dom'
 import HomeScreen from './home/HomeScreen'
 import MyCardScreen from './myCard/MyCardScreen'
 import Contents from './Contents'
-import {PUBLIC_URL} from './constants'
 
 export default function App() {
     return (
@@ -15,16 +14,16 @@ export default function App() {
                     <div className={styles.headerContents}>
                         <img className={styles.logo} src={pokemonLogo} alt="pokemon-logo"/>
                         <div className={styles.headerButtonArea}>
-                            <a href="/" className={styles.headerButton}>ホーム</a>
-                            <a href="/my-card" className={styles.headerButton}>マイカードリスト</a>
+                            <Link to="/" className={styles.headerButton}>ホーム</Link>
+                            <Link to="/my-card" className={styles.headerButton}>マイカードリスト</Link>
                         </div>
                     </div>
                 </Contents>
             </header>
 
             <Routes>
-                <Route path={`${PUBLIC_URL}/`} element={<HomeScreen/>}/>
-                <Route path={`${PUBLIC_URL}/my-card`} element={<MyCardScreen/>}/>
+                <Route path="/" element={<HomeScreen/>}/>
+                <Route path="/my-card" element={<MyCardScreen/>}/>
             </Routes>
         </>
     )
--- src/AppNavigation.tsx
+++ src/AppNavigation.tsx
@@ -1,9 +1,14 @@
 import {BrowserRouter} from 'react-router-dom'
 import App from './App'
 import {PUBLIC_URL} from './constants'
+import {useEffect} from 'react'
 
 
 export default function AppNavigation() {
+    useEffect(() => {
+        console.log(PUBLIC_URL)
+    }, [])
+
     return (
         <BrowserRouter basename={PUBLIC_URL}>
             <App/>

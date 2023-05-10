16ba52e5d6d11d15debed7e8e50d3a7acffab3b1
Yuta Shoji <yuta_shoji@mail.toyota.co.jp>
2023-05-10 18:34:33 +0900

    パックの種類と値段を確認することができる
    Signed-off-by: Kazuyoshi Akaishi <kazuyoshi_akaishi@mail.toyota.co.jp>
    Co-authored-by: Kazuyoshi Akaishi <kazuyoshi_akaishi@mail.toyota.co.jp>
    Co-authored-by: Junji Asai <jiyunji_asai@mail.toyota.co.jp>
    Co-authored-by: Shion Koga <shion_koga@mail.toyota.co.jp>

--- src/home/HomeScreen.tsx
+++ src/home/HomeScreen.tsx
@@ -1,10 +1,28 @@
 import styles from './HomeScreen.module.scss'
 import Contents from '../Contents'
+import pack1 from "../images/pack_ver1.png"
+import pack2 from "../images/pack_ver2.jpg"
+import pack3 from "../images/pack_ver3.jpeg"
+import pack4 from "../images/pack_ver4.jpg"
+import pack5 from "../images/pack_ver5.jpeg"
+import pack6 from "../images/pack_ver6.png"
 
 export default function HomeScreen() {
     return (
         <Contents>
             <h1 className={styles.title}>Pokemon Vending Machine</h1>
+            <img src={pack1} alt="拡張パック 20th Anniversary 10枚入り"/>
+            <div>￥260</div>
+            <img src={pack2} alt="neo 金,銀,新世界へ•••"/>
+            <div>￥180</div>
+            <img src={pack3} alt="さいはての攻防 拡張パック"/>
+            <div>￥290</div>
+            <img src={pack4} alt="DP 時空の創造"/>
+            <div>￥310</div>
+            <img src={pack5} alt="ブラックコレクション拡張パック"/>
+            <div>￥170</div>
+            <img src={pack6} alt="ソード&シールド Vmaxライジング 強化拡張パック"/>
+            <div>￥410</div>
         </Contents>
     )
 }
---
+++ src/tests/home/HomeScreen.test.tsx
@@ -0,0 +1,59 @@
+import {screen} from '@testing-library/react'
+import renderApplication from '../RenderApplication'
+
+describe('', () => {
+    test('購入画面のページタイトルが表示されている', async () => {
+        await renderApplication('/')
+
+
+        expect(screen.getByText('Pokemon Vending Machine')).toBeInTheDocument()
+    })
+
+    test('「拡張パック 20th Anniversary 10枚入り」パックの画像と値段がある', async () => {
+        await renderApplication('/')
+
+
+        expect(screen.getByText("￥260"))
+        expect(screen.getByAltText('拡張パック 20th Anniversary 10枚入り')).toBeInTheDocument()
+    })
+
+    test('「neo 金,銀,新世界へ•••」パックの画像と値段がある', async () => {
+        await renderApplication('/')
+
+
+        expect(screen.getByText("￥180"))
+        expect(screen.getByAltText('neo 金,銀,新世界へ•••')).toBeInTheDocument()
+    })
+
+    test('「さいはての攻防 拡張パック」パックの画像と値段がある', async () => {
+        await renderApplication('/')
+
+
+        expect(screen.getByText("￥290"))
+        expect(screen.getByAltText('さいはての攻防 拡張パック')).toBeInTheDocument()
+    })
+
+    test('「DP 時空の創造」パックの画像と値段がある', async () => {
+        await renderApplication('/')
+
+
+        expect(screen.getByText("￥310"))
+        expect(screen.getByAltText('DP 時空の創造')).toBeInTheDocument()
+    })
+
+    test('「ブラックコレクション拡張パック」パックの画像と値段がある', async () => {
+        await renderApplication('/')
+
+
+        expect(screen.getByText("￥170"))
+        expect(screen.getByAltText('ブラックコレクション拡張パック')).toBeInTheDocument()
+    })
+
+    test('「ソード&シールド Vmaxライジング 強化拡張パック」パックの画像と値段がある', async () => {
+        await renderApplication('/')
+
+
+        expect(screen.getByText("￥410"))
+        expect(screen.getByAltText('ソード&シールド Vmaxライジング 強化拡張パック')).toBeInTheDocument()
+    })
+})
